73275112c2fc5e96af478606d32c97d15c2a3750
Sławomir Gnatoński <slawek.gnatonski@gmail.com>
2018-06-19 00:36:23 +0200

    extracted parsing logic to separate classes and added command caching

--- Sorting/BaseSorting.cs
+++ Sorting/BaseSorting.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -11,16 +10,6 @@ namespace Slavapp.Extensions.EF.Sorting
         private Hashtable expressions = new Hashtable();
         private Hashtable complexExpressions = new Hashtable();
         private ISymbols symbols = new DefaultSymbols();
-
-        private IEnumerable<SortCommand> ParseCommands(string sortString)
-        {
-            var cols = sortString.Split(new[] { this.symbols.ColumnSeparator }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var col in cols)
-            {
-                var sort = col.Split(new[] { this.symbols.ValueSeparator }, StringSplitOptions.None);
-                yield return new SortCommand() { Column = sort[0], Ascending = sort[1] == this.symbols.SortAscending };
-            }
-        }
 
         private IOrderedQueryable<TModel> Order<TKey>(IQueryable<TModel> source, Expression<Func<TModel, TKey>> selector, bool ascending)
         {
@@ -34,11 +23,26 @@ namespace Slavapp.Extensions.EF.Sorting
             }
         }
 
+        protected void SetSorting<TReturn>(string name, Expression<Func<TModel, TReturn>> propertyExpression)
+        {
+            expressions.Add(name, propertyExpression);
+        }
+
+        protected void SetComplexSorting<TReturn>(string name, Func<string, TReturn> valueExtractor, Func<TReturn, Expression<Func<TModel, bool>>> propertyExpression)
+        {
+            complexExpressions.Add(name, Tuple.Create(valueExtractor, propertyExpression));
+        }
+
+        protected void SetSymbols(ISymbols symbols)
+        {
+            this.symbols = symbols ?? throw new ArgumentNullException(nameof(symbols));
+        }
+
         public IQueryable<T> Sort<T>(IQueryable<T> set, string sort)
         {
             var internalSet = set.Cast<TModel>();
             var sorted = false;
-            foreach (var command in ParseCommands(sort))
+            foreach (var command in SortingParser.ParseCommands(sort, this.symbols))
             {
                 var expr = expressions[command.Column];
                 if (expr != null)
@@ -50,14 +54,14 @@ namespace Slavapp.Extensions.EF.Sorting
                 {
                     foreach(var key in complexExpressions.Keys)
                     {
-                        if (command.Column.StartsWith(key.ToString())
-                            && complexExpressions[key] is Tuple<Func<string, object>, Func<object, Expression<Func<TModel, bool>>>> func)
+                        if (command.Column.StartsWith(key.ToString()))
                         {
+                            var func = (dynamic)complexExpressions[key];
                             var val = func.Item1(command.Column);
                             var expr2 = func.Item2(val);
                             if (expr2 != null)
                             {
-                                internalSet = Order(internalSet, (dynamic)expr2, command.Ascending);
+                                internalSet = Order(internalSet, expr2, command.Ascending);
                                 sorted = true;
                                 break;
                             }
@@ -70,21 +74,6 @@ namespace Slavapp.Extensions.EF.Sorting
                 internalSet = Order(internalSet, x => x != null, true);
             }
             return internalSet.Cast<T>();
-        }
-
-        protected void SetSorting<TReturn>(string name, Expression<Func<TModel, TReturn>> propertyExpression)
-        {
-            expressions.Add(name, propertyExpression);
-        }
-
-        protected void SetComplexSorting(string name, Func<string, object> valueExtractor, Func<object, Expression<Func<TModel, bool>>> propertyExpression)
-        {
-            complexExpressions.Add(name, Tuple.Create(valueExtractor, propertyExpression));
-        }
-
-        protected void SetSymbols(ISymbols symbols)
-        {
-            this.symbols = symbols ?? throw new ArgumentNullException(nameof(symbols));
         }
     }
 }
