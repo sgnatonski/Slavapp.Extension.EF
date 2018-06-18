8c6378c7237ada1291b3a4f2fc536934bf0354d0
Sławomir Gnatoński <slawek.gnatonski@gmail.com>
2018-06-18 20:12:22 +0200

    added possibility to sort collection by complex expression which value depends on specific sort query

--- Sorting/BaseSorting.cs
+++ Sorting/BaseSorting.cs
@@ -9,6 +9,7 @@ namespace Slavapp.Extensions.EF.Sorting
     public abstract class BaseSorting<TModel> : IBaseSorting
     {
         private Hashtable expressions = new Hashtable();
+        private Hashtable complexExpressions = new Hashtable();
         private ISymbols symbols = new DefaultSymbols();
 
         private IEnumerable<SortCommand> ParseCommands(string sortString)
@@ -36,20 +37,49 @@ namespace Slavapp.Extensions.EF.Sorting
         public IQueryable<T> Sort<T>(IQueryable<T> set, string sort)
         {
             var internalSet = set.Cast<TModel>();
+            var sorted = false;
             foreach (var command in ParseCommands(sort))
             {
-                var expr = expressions[command.Column];                
+                var expr = expressions[command.Column];
                 if (expr != null)
                 {
                     internalSet = Order(internalSet, (dynamic)expr, command.Ascending);
+                    sorted = true;
                 }
+                else
+                {
+                    foreach(var key in complexExpressions.Keys)
+                    {
+                        if (command.Column.StartsWith(key.ToString())
+                            && complexExpressions[key] is Tuple<Func<string, object>, Func<object, Expression<Func<TModel, bool>>>> func)
+                        {
+                            var val = func.Item1(command.Column);
+                            var expr2 = func.Item2(val);
+                            if (expr2 != null)
+                            {
+                                internalSet = Order(internalSet, (dynamic)expr2, command.Ascending);
+                                sorted = true;
+                                break;
+                            }
+                        }                        
+                    }
+                }
+            }
+            if (!sorted)
+            {
+                internalSet = Order(internalSet, x => x != null, true);
             }
             return internalSet.Cast<T>();
         }
 
-        protected void SetSorting<TReturn>(string name, Expression<Func<TModel, TReturn>> property)
+        protected void SetSorting<TReturn>(string name, Expression<Func<TModel, TReturn>> propertyExpression)
         {
-            expressions.Add(name, property);
+            expressions.Add(name, propertyExpression);
+        }
+
+        protected void SetComplexSorting(string name, Func<string, object> valueExtractor, Func<object, Expression<Func<TModel, bool>>> propertyExpression)
+        {
+            complexExpressions.Add(name, Tuple.Create(valueExtractor, propertyExpression));
         }
 
         protected void SetSymbols(ISymbols symbols)
